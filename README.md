7a801ae18d49850d78d528e94622db6694ae32fd
Matt Steiner <msteiner@ibidata.com>
2022-05-23 20:44:55 -0500

    remove button from admin bar, add simple readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# WPStarter
+
+A WordPress starter heavily inspired by [WordPressify](https://www.wordpressify.co/).
+
+
+Requirements:
+ - [PHP](https://www.php.net/)
+ - [MySQL](https://www.mysql.com/)
+ - [wp-cli](https://wp-cli.org/)
+
+I'll fill this out later...
--- src/theme/inc/disable/comments.php
+++ src/theme/inc/disable/comments.php
@@ -1,7 +1,7 @@
 <?php
 
 // Disable support for comments and trackbacks in post types
-function df_disable_comments_post_types_support() {
+function disable_comments_post_types_support() {
     $post_types = get_post_types();
     foreach ($post_types as $post_type) {
         if (post_type_supports($post_type, 'comments')) {
@@ -10,38 +10,50 @@ function df_disable_comments_post_types_support() {
         }
     }
 }
-add_action('admin_init', 'df_disable_comments_post_types_support');
+add_action('admin_init', 'disable_comments_post_types_support');
+
+
 // Close comments on the front-end
-function df_disable_comments_status() {
+function disable_comments_status() {
     return false;
 }
-add_filter('comments_open', 'df_disable_comments_status', 20, 2);
-add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+add_filter('comments_open', 'disable_comments_status', 20, 2);
+add_filter('pings_open', 'disable_comments_status', 20, 2);
+
+
 // Hide existing comments
-function df_disable_comments_hide_existing_comments($comments) {
+function disable_comments_hide_existing_comments($comments) {
     $comments = array();
     return $comments;
 }
-add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+add_filter('comments_array', 'disable_comments_hide_existing_comments', 10, 2);
+
+
 // Remove comments page in menu
-function df_disable_comments_admin_menu() {
+function disable_comments_admin_menu() {
     remove_menu_page('edit-comments.php');
 }
-add_action('admin_menu', 'df_disable_comments_admin_menu');
+add_action('admin_menu', 'disable_comments_admin_menu');
+
+
 // Redirect any user trying to access comments page
-function df_disable_comments_admin_menu_redirect() {
+function disable_comments_admin_menu_redirect() {
     global $pagenow;
     if ($pagenow === 'edit-comments.php') {
         wp_redirect(admin_url());
         exit;
     }
 }
-add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+add_action('admin_init', 'disable_comments_admin_menu_redirect');
+
+
 // Remove comments metabox from dashboard
-function df_disable_comments_dashboard() {
+function disable_comments_dashboard() {
     remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
 }
-add_action('admin_init', 'df_disable_comments_dashboard');
+add_action('admin_init', 'disable_comments_dashboard');
+
+
 // Remove comments links from admin bar
 function df_disable_comments_admin_bar() {
     if (is_admin_bar_showing()) {
@@ -49,3 +61,11 @@ function df_disable_comments_admin_bar() {
     }
 }
 add_action('init', 'df_disable_comments_admin_bar');
+
+
+// Remove comments button from admin bar
+function my_admin_bar_render() {
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+}
+add_action('wp_before_admin_bar_render', 'my_admin_bar_render');
