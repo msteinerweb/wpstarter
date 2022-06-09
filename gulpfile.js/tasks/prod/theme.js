d615c851ac99e15e2a9d00200af8d057d41f99cc
Matt Steiner <msteiner@ibidata.com>
2022-06-09 15:41:11 -0500

    css and js will be timestamped on build to prevent caching

--- gulpfile.js/tasks/prod/theme.js
+++ gulpfile.js/tasks/prod/theme.js
@@ -1,10 +1,23 @@
 const { site } = require("../../../config");
 const { src, dest } = require('gulp');
+const gulpReplace = require('gulp-replace');
 
 function theme() {
-    return src(['./src/theme/**', '!./src/theme/**/node_modules/**']).pipe(
-        dest(`./dist/themes/${site.theme_name}`),
+    return src(['./src/theme/**', '!./src/theme/**/node_modules/**'])
+        .pipe(gulpReplace('{{VERSION}}', timeToVersion()))
+        .pipe(dest(`./dist/themes/${site.theme_name}`)
     );
+}
+
+function timeToVersion() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hour = date.getHours().toString().padStart(2, '0');
+    const minute = date.getMinutes().toString().padStart(2, '0');
+    const second = date.getSeconds().toString().padStart(2, '0');
+    return `${year}-${month}-${day}_${hour}:${minute}:${second}`;
 }
 
 module.exports = theme;
--- src/theme/functions.php
+++ src/theme/functions.php
@@ -8,8 +8,8 @@ add_theme_support('post-thumbnails');
 
 // load theme scripts and styles
 function include_resources() {
-  wp_enqueue_style('style', get_stylesheet_uri(), array(), '20200420.1', 'all');
-  wp_enqueue_script('main_js', get_template_directory_uri() . '/js/main.js', null, '20200420.1', true);
+  wp_enqueue_style('style', get_stylesheet_uri(), array(), '{{VERSION}}', 'all');
+  wp_enqueue_script('main_js', get_template_directory_uri() . '/js/main.js', null, '{{VERSION}}', true);
 }
 add_action('wp_enqueue_scripts', 'include_resources');
 
