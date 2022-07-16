34a281062b0f0800f100b0b9973c67c03ae18dd3
Matt Steiner <msteiner@ibidata.com>
2022-07-16 15:43:42 -0500

    more btn updates

--- src/theme/header.php
+++ src/theme/header.php
@@ -13,6 +13,8 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=yes">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <?php wp_head(); ?>
 </head>
 
