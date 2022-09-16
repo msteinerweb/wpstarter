ccc8e5082dab9cf8e16bcad87967f1fa406a490d
Matt Steiner <msteiner@ibidata.com>
2022-09-16 15:19:30 -0500

    clean up resources

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -8,11 +8,13 @@ add_theme_support('post-thumbnails');
 
 // load theme scripts and styles
 function include_resources() {
-    wp_enqueue_style('style', get_stylesheet_uri(), [], '{{VERSION}}', 'all');
-    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', ['jquery'], '{{VERSION}}', true);
+
+    // custom styles / scripts
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '{{VERSION}}', 'all');
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), '{{VERSION}}', true);
+
+    // include fontawesome
+    wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css', array(), '5.15.4', 'all');
+
 }
 add_action('wp_enqueue_scripts', 'include_resources');
-
-
-// include api routes
-// require_once(dirname(__FILE__) . '/api/index.php');
