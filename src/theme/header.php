0e4eb3b8d0d467ad19e7554d8f843d0b49815ca2
Matt Steiner <msteiner@ibidata.com>
2022-09-17 10:15:12 -0500

    update head tags

--- src/theme/header.php
+++ src/theme/header.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title(' | ', 'echo', 'right'); ?></title>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=5.0,user-scalable=yes">
@@ -17,3 +15,4 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
