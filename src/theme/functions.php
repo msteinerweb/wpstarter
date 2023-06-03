17bbb2af31aa8f00a1fe0b18bfeeed17f315b05c
Matt Steiner <msteiner@ibidata.com>
2023-06-03 15:16:48 -0500

    add additional theme support

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,9 +1,14 @@
 <?php
 
-// setup thumbnail / featured image support
-add_theme_support('custom-logo');
-add_theme_support('post-thumbnails');
-// add_image_size('resource-thumbnail', 400, 250, true);
+// theme support
+function theme_setup() {
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+    add_theme_support('title-tag');
+    // add_image_size('resource-thumbnail', 400, 250, true);
+}
+add_action('after_setup_theme', 'theme_setup');
 
 
 // load theme scripts and styles
