a57c7e11abce9b6187639b93f06a040077e0868b
Matt Steiner <msteiner@ibidata.com>
2023-08-08 15:44:55 -0500

    include css gulp file

--- gulpfile.js/prod/css.js
+++ gulpfile.js/prod/css.js
@@ -1,6 +1,7 @@
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const dart = require('dart-sass');
+const cssnano = require('gulp-cssnano');
 const gulpHeader = require('gulp-header');
 const gulpReplace = require('gulp-replace');
 const timeToVersion = require('../util/timeToVersion');
@@ -12,7 +13,10 @@ sass.compiler = dart;
 
 function css() {
     return src('./src/assets/scss/style.scss')
-        .pipe(sass({ includePaths: 'node_modules' }))
+        .pipe(sass({
+            includePaths: 'node_modules',
+        }))
+        .pipe(cssnano())
         .pipe(gulpHeader(themeComment))
         .pipe(gulpReplace('{{VERSION}}', timeToVersion()))
         .pipe(dest(`./dist/themes/${site.theme_name}`));
