285d1c1063979f4ce38a555ffe818db0114e3722
Matt Steiner <msteiner@ibidata.com>
2023-08-08 18:24:58 -0500

    imporve eslint

--- src/blocks/example.jsx
+++ src/blocks/example.jsx
@@ -2,9 +2,9 @@ wp.blocks.registerBlockType('wpstarter/example', {
     title: 'Example',
     icon: 'smiley',
     category: 'common',
-    edit: function() {
+    edit: function () {
         return (
             <p className="example">Hello World!</p>
         );
-    }
+    },
 });
