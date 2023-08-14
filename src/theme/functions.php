c4fb90478f4565efdefd35cf83ef8ac252aa4363
Matt Steiner <msteiner@ibidata.com>
2023-08-14 08:55:17 -0500

    add align-wide support

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -2,11 +2,12 @@
 
 // theme support
 function theme_setup() {
+    // add_image_size('resource-thumbnail', 400, 250, true);
     add_theme_support('custom-logo');
     add_theme_support('post-thumbnails');
+    add_theme_support('align-wide');
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
     add_theme_support('title-tag');
-    // add_image_size('resource-thumbnail', 400, 250, true);
     add_theme_support('editor-styles');
     add_editor_style('style.css');
 }
